40dffce0813f2da61448d45af47d7e4109fbf5fb
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-11 15:45:03 +0100

    object added

---
+++ scripts/db.js
@@ -0,0 +1,62 @@
+let options = [
+  {
+    pizza: [
+      {
+        name: "Pizza Teriyaki",
+        description: "mit Hähnchen, extra scharf",
+        price: "16,99",
+      },
+      {
+        name: "Pizza Funghi",
+        description: "mit frischen Pilzen",
+        price: "13,99",
+      },
+      {
+        name: "Pizza Hawaii",
+        description: "mit Ananas und Hinterschinken",
+        price: "14,99",
+      },
+      {
+        name: "Pizza Sucuk & Tonno",
+        description: "mit Knoblauchsalami, Thunfisch und Jalapenos",
+        price: "16,99",
+      },
+    ],
+
+    pasta: [
+      {
+        name: "Pasta Arrabiata",
+        description: "extra scharf",
+        price: "13,99",
+      },
+      {
+        name: "Pasta al Salmone",
+        description: "mit frischem Lachs",
+        price: "16,99",
+      },
+      {
+        name: "Pasta Carbonara",
+        description: "original italienisch mit Speck, Parmesan und Ei",
+        price: "13,99",
+      },
+    ],
+
+    desert: [
+      {
+        name: "Tiramisu",
+        description: "ohne Alkohol",
+        price: "7,99",
+      },
+      {
+        name: "Panna Cotta",
+        description: "mit Himbeersoße",
+        price: "7,99",
+      },
+      {
+        name: "Crème Brûlée",
+        description: "",
+        price: "7,99",
+      },
+    ],
+  },
+];
