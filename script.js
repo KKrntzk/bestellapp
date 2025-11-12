383f8bd465924e22f264393a5c70c1781b8b6d1a
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-12 15:08:52 +0100

    basic function expanded

--- script.js
+++ script.js
@@ -4,8 +4,12 @@
 
 function renderEachElement() {
   let pizzaTargetRef = document.getElementById("pizzaVariationTarget");
+  let pastaTargetRef = document.getElementById("pastaVariationTarget");
+  let desertTargetRef = document.getElementById("desertVariationTarget");
   for (let index = 0; index < options.length; index++) {
     pizzaTargetRef.innerHTML += renderEachPizza(index);
+    pastaTargetRef.innerHTML += renderEachPasta(index);
+    desertTargetRef.innerHTML += renderEachDesert(index);
   }
 }
 
@@ -45,6 +49,90 @@ function renderSinglePizza(
                 <p id="variationName(${pizzaIndex})">${elementPizzaName}</p>
                 <p id="variationDescription(${pizzaIndex})">${elementPizzaDescription}</p>
                 <p id="variationPrice(${pizzaIndex})">${elementPizzaPrice}</p>
+              </section>
+                <button id="addBtn" class="add-btn">+</button>
+            </section>`;
+}
+
+//#endregion
+
+//#region pasta functions
+function renderEachPasta(index) {
+  const pastaVariationTargetRef = document.getElementById(
+    "pastaVariationTarget"
+  );
+  pastaVariationTargetRef.innerHTML = "";
+  for (
+    let pastaIndex = 0;
+    pastaIndex < options[index].pasta.length;
+    pastaIndex++
+  ) {
+    const elementPastaName = options[index].pasta[pastaIndex].name;
+    const elementPastaPrice = options[index].pasta[pastaIndex].price;
+    const elementPastaDescription =
+      options[index].pasta[pastaIndex].description;
+    pastaVariationTargetRef.innerHTML += renderSinglePasta(
+      pastaIndex,
+      elementPastaName,
+      elementPastaPrice,
+      elementPastaDescription
+    );
+  }
+}
+
+function renderSinglePasta(
+  pastaIndex,
+  elementPastaName,
+  elementPastaPrice,
+  elementPastaDescription
+) {
+  return `  <section class="variation">
+              <section class="variation-specs">
+                <p id="variationName(${pastaIndex})">${elementPastaName}</p>
+                <p id="variationDescription(${pastaIndex})">${elementPastaDescription}</p>
+                <p id="variationPrice(${pastaIndex})">${elementPastaPrice}</p>
+              </section>
+                <button id="addBtn" class="add-btn">+</button>
+            </section>`;
+}
+
+//#endregion
+
+//#region desert functions
+function renderEachDesert(index) {
+  const desertVariationTargetRef = document.getElementById(
+    "desertVariationTarget"
+  );
+  desertVariationTargetRef.innerHTML = "";
+  for (
+    let desertIndex = 0;
+    desertIndex < options[index].desert.length;
+    desertIndex++
+  ) {
+    const elementDesertName = options[index].desert[desertIndex].name;
+    const elementDesertPrice = options[index].desert[desertIndex].price;
+    const elementDesertDescription =
+      options[index].desert[desertIndex].description;
+    desertVariationTargetRef.innerHTML += renderSingleDesert(
+      desertIndex,
+      elementDesertName,
+      elementDesertPrice,
+      elementDesertDescription
+    );
+  }
+}
+
+function renderSinglePasta(
+  desertIndex,
+  elementDesertName,
+  elementDesertPrice,
+  elementDesertDescription
+) {
+  return `  <section class="variation">
+              <section class="variation-specs">
+                <p id="variationName(${desertIndex})">${elementDesertName}</p>
+                <p id="variationDescription(${desertIndex})">${elementDesertDescription}</p>
+                <p id="variationPrice(${desertIndex})">${elementDesertPrice}</p>
               </section>
                 <button id="addBtn" class="add-btn">+</button>
             </section>`;
