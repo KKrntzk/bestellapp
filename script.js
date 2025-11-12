cb39c9c5e0477e398df3baa9dc8451ddf8559a92
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-12 14:22:21 +0100

    basic functions added

--- script.js
+++ script.js
@@ -2,4 +2,53 @@
 
 //function to render in each dish info
 
+function renderEachElement() {
+  let pizzaTargetRef = document.getElementById("pizzaVariationTarget");
+  for (let index = 0; index < options.length; index++) {
+    pizzaTargetRef.innerHTML += renderEachPizza(index);
+  }
+}
+
+//#region pizza functions
+
+function renderEachPizza(index) {
+  const pizzaVariationTargetRef = document.getElementById(
+    "pizzaVariationTarget"
+  );
+  pizzaVariationTargetRef.innerHTML = "";
+  for (
+    let pizzaIndex = 0;
+    pizzaIndex < options[index].pizza.length;
+    pizzaIndex++
+  ) {
+    const elementPizzaName = options[index].pizza[pizzaIndex].name;
+    const elementPizzaPrice = options[index].pizza[pizzaIndex].price;
+    const elementPizzaDescription =
+      options[index].pizza[pizzaIndex].description;
+    pizzaVariationTargetRef.innerHTML += renderSinglePizza(
+      pizzaIndex,
+      elementPizzaName,
+      elementPizzaPrice,
+      elementPizzaDescription
+    );
+  }
+}
+
+function renderSinglePizza(
+  pizzaIndex,
+  elementPizzaName,
+  elementPizzaPrice,
+  elementPizzaDescription
+) {
+  return `  <section class="variation">
+              <section class="variation-specs">
+                <p id="variationName(${pizzaIndex})">${elementPizzaName}</p>
+                <p id="variationDescription(${pizzaIndex})">${elementPizzaDescription}</p>
+                <p id="variationPrice(${pizzaIndex})">${elementPizzaPrice}</p>
+              </section>
+                <button id="addBtn" class="add-btn">+</button>
+            </section>`;
+}
+
+//#endregion
 //#endregion
