4459e01242d37ee9b5c3b7783b75a7db735bdfba
KKrntzk <sophie.kornetzki@gmail.com>
2025-11-13 08:06:06 +0100

    css updates

--- script.js
+++ script.js
@@ -36,9 +36,9 @@ function renderSinglePizza(
 ) {
   return `  <section class="variation">
               <section class="variation-specs">
-                <p id="variationName(${pizzaIndex})">${elementPizzaName}</p>
+                <p id="variationName(${pizzaIndex})"><strong>${elementPizzaName}</strong></p>
                 <p id="variationDescription(${pizzaIndex})">${elementPizzaDescription}</p>
-                <p id="variationPrice(${pizzaIndex})">${elementPizzaPrice}</p>
+                <p id="variationPrice(${pizzaIndex})">${elementPizzaPrice}</p><br>
               </section>
                 <button id="addBtn" class="add-btn">+</button>
             </section>`;
@@ -73,9 +73,9 @@ function renderSinglePasta(
 ) {
   return `  <section class="variation">
               <section class="variation-specs">
-                <p id="variationName(${pastaIndex})">${elementPastaName}</p>
+                <p id="variationName(${pastaIndex})"><strong>${elementPastaName}</strong></p>
                 <p id="variationDescription(${pastaIndex})">${elementPastaDescription}</p>
-                <p id="variationPrice(${pastaIndex})">${elementPastaPrice}</p>
+                <p id="variationPrice(${pastaIndex})">${elementPastaPrice}</p><br>
               </section>
                 <button id="addBtn" class="add-btn">+</button>
             </section>`;
@@ -114,9 +114,9 @@ function renderSingleDesert(
 ) {
   return `  <section class="variation">
               <section class="variation-specs">
-                <p id="variationName(${desertIndex})">${elementDesertName}</p>
+                <p id="variationName(${desertIndex})"><strong>${elementDesertName}</strong></p>
                 <p id="variationDescription(${desertIndex})">${elementDesertDescription}</p>
-                <p id="variationPrice(${desertIndex})">${elementDesertPrice}</p>
+                <p id="variationPrice(${desertIndex})">${elementDesertPrice}</p><br>
               </section>
                 <button id="addBtn" class="add-btn">+</button>
             </section>`;
